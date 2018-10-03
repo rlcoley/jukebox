4476b7d189885c960b44e87a0059a2a81ddc819a
rlcoley <rosscoley1002@gmail.com>
2018-10-03 17:11:04 -0400

    put each song in song folder

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ var songTitle = document.getElementById('songTitle')
 
 
 
-var s1 = { url: new Audio('lordknows.mp3'), title: 'Lord Knows', cover: "'lordknowsalbumcover.png'"}
-var s2 = { url: new Audio('donotdistrub.mp3'), title: 'Do Not Disturb', cover: "'drakealbumcover.jpg'"}
-var s3 = { url: new Audio('patrick.mp3'), title: "Patrick's M.A.A.D City",cover: "'patrickalbumcover.jpg'" }
-var s4 = { url: new Audio('superduperkyle.mp3'), title: 'SuperDuperKyle', cover: "'kylealbumcover.jpg'"}
-var s5 = {url: new Audio('paradiseextended.mp3'),title:'Paradise Extended', cover: "'bigseanalbumcover.jpeg'" }
-var s6 = {url: new Audio('bigbank.mp3'),title:'Big Bank',cover: "'bigbankalbumcover.jpg'" }
-var s7 = {url: new Audio('losecontrol.mp3'),title:'Lose Control', cover: "'losecontrolalbum.jpg'" }
+var s1 = { url: new Audio('songs/lordknows.mp3'), title: 'Lord Knows', cover: "'lordknowsalbumcover.png'"}
+var s2 = { url: new Audio('songs/donotdistrub.mp3'), title: 'Do Not Disturb', cover: "'drakealbumcover.jpg'"}
+var s3 = { url: new Audio('songs/patrick.mp3'), title: "Patrick's M.A.A.D City",cover: "'patrickalbumcover.jpg'" }
+var s4 = { url: new Audio('songs/superduperkyle.mp3'), title: 'SuperDuperKyle', cover: "'kylealbumcover.jpg'"}
+var s5 = {url: new Audio('songs/paradiseextended.mp3'),title:'Paradise Extended', cover: "'bigseanalbumcover.jpeg'" }
+var s6 = {url: new Audio('songs/bigbank.mp3'),title:'Big Bank',cover: "'bigbankalbumcover.jpg'" }
+var s7 = {url: new Audio('songs/losecontrol.mp3'),title:'Lose Control', cover: "'losecontrolalbum.jpg'" }
 var listOfSongs = [s1, s2, s3 ,s4, s5, s6, s7]
 var trackNum = 0;
 
@@ -29,7 +29,7 @@ function playFunction() {
   listOfSongs[trackNum].url.play()
   songTitle.innerHTML = listOfSongs[trackNum].title
   albumCover.style.backgroundImage = "url("+listOfSongs[trackNum].cover +")";
-  // trackNum ++;
+  albumCover.style.backgroundImageSize = "cover";
 }
 
 function nextSong() {
