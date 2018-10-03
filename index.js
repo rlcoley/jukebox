c698628d2b05fdfdcd3d90fc14c469fab02e1b03
rlcoley <rosscoley1002@gmail.com>
2018-10-03 16:53:45 -0400

    albumcover id, var and key/vaule. Added each album cover with each control button

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
+// controls
 var play = document.getElementById('play')
 var pause = document.getElementById('pause')
 var next = document.getElementById('next')
-var songTitle = document.getElementById('songTitle')
 var repeat = document.getElementById('repeat')
 var previous = document.getElementById('previousSong')
 
-var s1 = { url: new Audio('lordknows.mp3'), title: 'Lord Knows'}
-var s2 = { url: new Audio('donotdistrub.mp3'), title: 'Do Not Disturb'}
-var s3 = { url: new Audio('patrick.mp3'), title: "Patrick's M.A.A.D City" }
-var s4 = { url: new Audio('superduperkyle.mp3'), title: 'SuperDuperKyle'}
-var s5 = {url: new Audio('paradiseextended.mp3'),title:'Paradise Extended' }
+// Title of song
+var songTitle = document.getElementById('songTitle')
 
-var listOfSongs = [s1, s2, s3 ,s4, s5]
+// Album cover
+ var albumCover = document.getElementById('albumCover')
+
+
+
+var s1 = { url: new Audio('lordknows.mp3'), title: 'Lord Knows', cover: "'lordknowsalbumcover.png'"}
+var s2 = { url: new Audio('donotdistrub.mp3'), title: 'Do Not Disturb', cover: "'drakealbumcover.jpg'"}
+var s3 = { url: new Audio('patrick.mp3'), title: "Patrick's M.A.A.D City",cover: "'patrickalbumcover.jpg'" }
+var s4 = { url: new Audio('superduperkyle.mp3'), title: 'SuperDuperKyle', cover: "'kylealbumcover.jpg'"}
+var s5 = {url: new Audio('paradiseextended.mp3'),title:'Paradise Extended', cover: "'bigseanalbumcover.jpeg'" }
+var s6 = {url: new Audio('bigbank.mp3'),title:'Big Bank',cover: "'bigbankalbumcover.jpg'" }
+var s7 = {url: new Audio('losecontrol.mp3'),title:'Lose Control', cover: "'losecontrolalbum.jpg'" }
+var listOfSongs = [s1, s2, s3 ,s4, s5, s6, s7]
 var trackNum = 0;
 
 
@@ -19,6 +28,7 @@ function playFunction() {
   // console.log(trackNum.url);
   listOfSongs[trackNum].url.play()
   songTitle.innerHTML = listOfSongs[trackNum].title
+  albumCover.style.backgroundImage = "url("+listOfSongs[trackNum].cover +")";
   // trackNum ++;
 }
 
@@ -32,9 +42,11 @@ function nextSong() {
     trackNum = 0
     listOfSongs[trackNum].url.play()
     songTitle.innerHTML = listOfSongs[trackNum].title
+    albumCover.style.backgroundImage = "url("+listOfSongs[trackNum].cover +")";
   }else {
     listOfSongs[trackNum].url.play()
     songTitle.innerHTML = listOfSongs[trackNum].title
+    albumCover.style.backgroundImage = "url("+listOfSongs[trackNum].cover +")";
   }
 }
 
@@ -48,6 +60,7 @@ function previousSong() {
   }
   listOfSongs[trackNum].url.play()
   songTitle.innerHTML = listOfSongs[trackNum].title
+  albumCover.style.backgroundImage = "url("+listOfSongs[trackNum].cover +")";
 
 }
 
